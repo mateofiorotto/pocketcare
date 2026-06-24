a088ca3220196afeca9689f384a19d66cdd337ee
Mateo Fiorotto <mateofiorotto2004@gmail.com>
2026-06-24 12:44:30 -0300

    initializing project

---
+++ src/main/java/com/mateofiorotto/pocketcare/PocketcareApplication.java
@@ -0,0 +1,13 @@
+package com.mateofiorotto.pocketcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateofiorotto/pocketcare/PocketcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateofiorotto.pocketcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
