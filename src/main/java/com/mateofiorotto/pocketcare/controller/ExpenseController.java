9a02a377a5fb2a6914bc2c4a50829caeb3d9fd40
Mateo Fiorotto <mateofiorotto2004@gmail.com>
2026-06-29 12:46:09 -0300

    feature(expenses): add controller methods

--- src/main/java/com/mateofiorotto/pocketcare/controller/ExpenseController.java
+++ src/main/java/com/mateofiorotto/pocketcare/controller/ExpenseController.java
@@ -1,8 +1,54 @@
 package com.mateofiorotto.pocketcare.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mateofiorotto.pocketcare.dto.expense.ExpenseRequestDTO;
+import com.mateofiorotto.pocketcare.dto.expense.ExpenseResponseDTO;
+import com.mateofiorotto.pocketcare.service.expense.IExpenseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/expenses")
 public class ExpenseController {
+    private final IExpenseService expenseService;
 
+    public ExpenseController(IExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ExpenseResponseDTO>> getExpensesList(){
+        return ResponseEntity.ok(expenseService.getExpensesList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ExpenseResponseDTO> getExpenseById(@PathVariable UUID id){
+        return ResponseEntity.ok(expenseService.getExpenseById(id));
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> saveExpense(@RequestBody ExpenseRequestDTO expense){
+
+        expenseService.createExpense(expense);
+
+        return ResponseEntity.ok("Expense saved successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> saveExpense(@PathVariable UUID id, @RequestBody ExpenseRequestDTO expense){
+
+        expenseService.updateExpense(id, expense);
+
+        return ResponseEntity.ok("Expense updated successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteExpense(@PathVariable UUID id){
+
+        expenseService.deleteExpense(id);
+
+        return ResponseEntity.ok("Expense deleted successfully");
+    }
 }
