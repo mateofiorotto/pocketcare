87932d71b0ec77f4e4c217895dfcc6a937264e34
Mateo Fiorotto <mateofiorotto2004@gmail.com>
2026-06-30 09:10:27 -0300

    feature(categories): add enum of categories

---
+++ src/main/java/com/mateofiorotto/pocketcare/entity/Category.java
@@ -0,0 +1,22 @@
+package com.mateofiorotto.pocketcare.entity;
+
+public enum Category {
+    FOOD,
+    TRANSPORT,
+    HOUSING,
+    UTILITIES,
+    HEALTH,
+    EDUCATION,
+    ENTERTAINMENT,
+    SHOPPING,
+    SPORTS,
+    TRAVEL,
+    SUBSCRIPTIONS,
+    INSURANCE,
+    SAVINGS,
+    GIFTS,
+    PETS,
+    PERSONAL_CARE,
+    TAXES,
+    OTHER
+}
